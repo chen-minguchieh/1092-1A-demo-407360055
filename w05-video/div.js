b2b6858f15420905e80a6a842cb110296767aff6
chen-minguchieh <e079519@gmail.com>
2021-04-18 23:17:26 +0800

    Initial commit, saving w01~w05 files

--- w05-video/div.js
+++ w05-video/div.js
@@ -1,4 +1,4 @@
-function changeVideo(index) {
+function changeVideo (index) {
     const player = document.querySelector("#player");
     console.log("player",player);
     switch (index) {
@@ -10,10 +10,10 @@ function changeVideo(index) {
             break;
         case 3:
             player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/METhdbL_iMw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-      break;
+            break;
         case 4:
-        player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/ZyDbq-lEKTo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-        break;
+            player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/ZyDbq-lEKTo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            break;
         case 5:
             player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/NlsrJbVvjaA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
             break;
@@ -21,14 +21,15 @@ function changeVideo(index) {
 }
 
 function changeColor(index) {
-    const p + document.querySelector('#section');
-    console.log('section',p);
+    const p = document.querySelector("#section");
+    console.log("section", p);
+
     switch (index) {
         case 1:
             p.style.backgroundColor = 'yellow';
             break; 
         case 2:
-            p.style.backgroundColor = 'yellow';
+            p.style.backgroundColor = 'red';
             break;
         case 3:
             p.style.backgroundColor = 'blue';
@@ -39,21 +40,22 @@ function changeColor(index) {
 function changeImage(index) {
     const pic = document.querySelector("#player");
     console.log("player",pic);
+
     switch (index) {
         case 1:
-            player.innerHTML =`<img src="./1.png" style="width: 100%; height: auto" alt="">`;
+            player.innerHTML =`<img src="./img1.png" style="width: 100%; height: auto" alt="">`;
       break;
         case 2:
-            player.innerHTML =`<img src="./2.png" style="width: 100%; height: auto" alt="">`;
+            player.innerHTML =`<img src="./img2.png" style="width: 100%; height: auto" alt="">`;
       break;
         case 3:
-            player.innerHTML =`<img src="./3.png" style="width: 100%; height: auto" alt="">`;
+            player.innerHTML =`<img src="./img3.png" style="width: 100%; height: auto" alt="">`;
       break;
         case 4:
-            player.innerHTML =`<img src="./4.png" style="width: 100%; height: auto" alt="">`;
+            player.innerHTML =`<img src="./img4.png" style="width: 100%; height: auto" alt="">`;
       break;
         case 5:
-            player.innerHTML =`<img src="./5.png" style="width: 100%; height: auto" alt="">`;
+            player.innerHTML =`<img src="./img5.png" style="width: 100%; height: auto" alt="">`;
       break;
     }
 }
